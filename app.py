4564f271dd525e0c898f1307cc1e7637a5600c43
konentung <84655730+konentung@users.noreply.github.com>
2024-06-18 22:56:16 +0800

    feature-拿掉sys

--- app.py
+++ app.py
@@ -17,20 +17,12 @@ from linebot.v3.messaging import (
     TextMessage
 )
 
-import sys
 import os
 
 app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
-
-if CHANNEL_SECRET is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if CHANNEL_ACCESS_TOKEN is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
     
 handler = WebhookHandler(CHANNEL_SECRET)
 
