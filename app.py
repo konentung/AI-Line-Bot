ee282b235c2b1e94f3fd6fe8e86f7e18081e7f8e
konentung <84655730+konentung@users.noreply.github.com>
2024-08-16 19:03:37 +0800

    feature-將GPT串接 1. 將reply_message的部分修正至適當的格式 2. 新增reply_GPT_message來添加GPT的回覆 3. 新增follow event

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from linebot.v3.exceptions import (
 )
 from linebot.v3.webhooks import (
     MessageEvent,
+    FollowEvent,
     TextMessageContent
 )
 from linebot.v3.messaging import (
@@ -14,15 +15,19 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
+    ShowLoadingAnimationRequest
 )
-
+from openai import OpenAI
+import anthropic
 import os
 
 app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
+Claude_API_KEY = os.getenv("API_KEY")
+GPT_API_KEY = os.getenv("GPT_API_KEY")
     
 line_handler = WebhookHandler(CHANNEL_SECRET)
 
@@ -47,8 +52,13 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessageContent)
 def handle_messsage(event):
-    messages = []
-    messages.append(TextMessage(text=event.message.text))
+    messages = [TextMessage(text=reply_GPT_message(event))]
+    # messages = [TextMessage(text=event.message.text)]
+    reply_message(event, messages)
+    
+@line_handler.add(FollowEvent)
+def handle_follow(event):
+    messages = [TextMessage(text="歡迎加入我的AI-LINE-BOT，您可以詢問我任何問題，我會盡力回答您！")]
     reply_message(event, messages)
     
 def reply_message(event, messages):
@@ -58,6 +68,38 @@ def reply_message(event, messages):
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=messages
+            )
+        )
+        
+def reply_GPT_message(event):
+    play_animation(event)
+    client = OpenAI(api_key=GPT_API_KEY)
+    user_message = event.message.text
+    
+    completion = client.chat.completions.create(
+        # model="gpt-4o-mini-2024-07-18",
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ],
+        max_tokens=1000,
+        temperature=0.2
+    )
+    return completion.choices[0].message.content
+    
+def reply_Claude_message(event):        
+        client = anthropic.Anthropic( api_key = Claude_API_KEY)
+        
+def play_animation(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.show_loading_animation(
+            ShowLoadingAnimationRequest(
+                chatId=event.source.user_id,
+                loadingSeconds=10
             )
         )
         
